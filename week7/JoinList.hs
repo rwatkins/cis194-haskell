dcb66bf9d355146948397f5343c0fcde7d9f2810
Riley Watkins <riley@rileywatkins.com>
2015-02-24 00:54:36 -0800

    Formatting

--- week7/JoinList.hs
+++ week7/JoinList.hs
@@ -96,10 +96,10 @@ instance Buffer (JoinList (Score, Size) String) where
     value (Append ((Score i), _) _ _) = i
 
 main :: IO ()
-main = runEditor editor
-           (fromString $ unlines
-            [ "This buffer is for notes you don't want to save, and for"
-            , "evaluation of steam valve coefficients."
-            , "To load a different file, type the character L followed"
-            , "by the name of the file."
-            ] :: (JoinList (Score, Size) String))
+main = runEditor editor buf
+    where buf = fromString $ unlines
+                [ "This buffer is for notes you don't want to save, and for"
+                , "evaluation of steam valve coefficients."
+                , "To load a different file, type the character L followed"
+                , "by the name of the file."
+                ] :: (JoinList (Score, Size) String)
